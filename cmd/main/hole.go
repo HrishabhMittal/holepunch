2083eb17c3913920f28791624d2940454f66f0bc
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-02-01 01:50:53 +0530

    add logs

--- cmd/main/hole.go
+++ cmd/main/hole.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net"
 	"time"
 )
@@ -8,11 +9,14 @@ func punchHole(con *net.UDPConn,addr *net.UDPAddr) {
 	buf := make([]byte, 128)
 	for {
 		con.SetReadDeadline(time.Now().Add(time.Second/2))
-		_, _, err := con.ReadFromUDP(buf)
+		n, _, err := con.ReadFromUDP(buf)
 		if err != nil {
+			fmt.Print(err)
 			return
 		}
+		fmt.Printf("read %d bytes\n",n)
 		con.WriteToUDP([]byte("hello"),addr)
+		fmt.Println("send hello to addr")
 	}
 }
 
