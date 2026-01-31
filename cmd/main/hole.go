477fc23b5a3daf798b4c1e81acdc18d8275764ab
HrishabhMittal <hrishabhmittal.hm@gmail.com>
2026-02-01 02:05:40 +0530

    fix send recv order

--- cmd/main/hole.go
+++ cmd/main/hole.go
@@ -5,19 +5,20 @@ import (
 	"net"
 	"time"
 )
-func punchHole(con *net.UDPConn,addr *net.UDPAddr) {
+func punchHole(con *net.UDPConn,addr *net.UDPAddr) (bool) {
 	buf := make([]byte, 128)
-	for {
+	// for {
+		con.WriteToUDP([]byte("hello"),addr)
+		fmt.Println("send hello to addr")
 		con.SetReadDeadline(time.Now().Add(time.Second/2))
 		n, _, err := con.ReadFromUDP(buf)
 		if err != nil {
 			fmt.Println("ERROR:")
 			fmt.Println(err)
-			return
+			return false
 		}
 		fmt.Printf("read %d bytes\n",n)
-		con.WriteToUDP([]byte("hello"),addr)
-		fmt.Println("send hello to addr")
-	}
+		return true
+	// }
 }
 
